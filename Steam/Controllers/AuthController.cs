64bfdc54a4444edfb84e795e16c57a603d91942b
Cherkashyn_Vitalii <vitaliic@ascmail.com>
2025-03-03 16:15:54 +0200

    add loggin

---
+++ Steam/Controllers/AuthController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Steam.Models;
+
+namespace Steam.Controllers
+{
+	public class AuthController : Controller
+	{
+		[HttpGet]
+		public IActionResult Login()
+		{
+			return View();
+		}		
+		[HttpPost]
+		public IActionResult Login(LoginModel model)
+		{
+			return Json("true");
+		}
+	}
+}
